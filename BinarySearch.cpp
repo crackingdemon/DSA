c5baf755a6aca5b5c341cb6de9beba9ab50805a7
crackingdemon <souravchauhanrajput@gmail.com>
2021-08-04 17:39:04 +0530

    sorting

---
+++ BinarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarysearch(int arr[], int n , int key ){
+int s =0;
+int e=n;
+while(s<=e){
+  int m =(s+e)/2;
+  if(arr[m]==key){
+    return m;
+  }
+  else if(arr[m]>key){
+    e=m-1;
+  }
+  else{
+    s=m+1;
+  }
+}
+
+}
+
+int main() {
+ int n ;
+ cin>>n;
+
+ int arr[n];
+ for(int i =0;i<n;i++){
+   cin>>arr[i];
+ }
+ int key;
+ 
+ cin>>key;
+
+ cout<<binarysearch(arr, n,key);
+ return 0;
+}
---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int arr[]={10,20,30,40,50};
+ for (int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+ }
+ return 0;
+}
