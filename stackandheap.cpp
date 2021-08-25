c3686939f17f3e7811608334c7437fdb4534f2a7
crackingdemon <souravchauhanrajput@gmail.com>
2021-08-25 16:13:15 +0530

    stackandheap

---
+++ stackandheap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+// Stack memory allocation -- 
+// The memory is allocated on the function call stack. The memory gets deallocated
+// as soon as the function call gets over. Deallocation is handled by the compiler.
+// Heap Memory Allocation
+// Allocation takes place on the pile of memory space available to programmers to
+// allocated and de-allocate. The programmer has to handle the deallocation.
+
+
+// To delte a memory pointed by pointer p 
+delete(p);
+
+//to form a new operator
+
+int* p = new int[9];
+
+    return 0 ;
+}
